a80122d1204b56132018a402defe4ff024f4b428
Amy <amymccarthy2014@gmail.com>
2021-11-23 19:36:57 +0000

    linted file

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,18 +2,18 @@ const request = require('request');
 
 const breed = process.argv.slice(2);
 
-const options = { 
+const options = {
   method: 'GET',
   url: `https://api.thecatapi.com/v1/breeds/search?q=${breed}`,
-  headers: { 'x-api-key': 'DEMO-API-KEY' } 
+  headers: { 'x-api-key': 'DEMO-API-KEY' }
 };
 
-request(options, function (error, response, body) {
+request(options, function(error, response, body) {
   if (error) {
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     return;
   }
   let breedInfo = JSON.parse(body); // asign variable to returned body info (returned as an array)
-  console.log(breedInfo[0].description); // access first item in array [0] to get description value of object 
+  console.log(breedInfo[0].description); // access first item in array [0] to get description value of object
 });
