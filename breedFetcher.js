51a501c377c02909095d529d24d1b4a7596942b5
Amy <amymccarthy2014@gmail.com>
2021-11-24 04:04:57 +0000

    wrap request in a function called fetchBreedDescription; export to index.js file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,21 @@
 const request = require('request');
 
-const breed = process.argv.slice(2);
+const fetchBreedDescription = function(breedName, callback) {
+  const options = {
+    method: 'GET',
+    url: `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    headers: { 'x-api-key': 'DEMO-API-KEY' }
+  };
 
-const options = {
-  method: 'GET',
-  url: `https://api.thecatapi.com/v1/breeds/search?q=${breed}`,
-  headers: { 'x-api-key': 'DEMO-API-KEY' }
+  request(options, function(error, response, body) {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    let breedInfo = JSON.parse(body); // asign variable to returned body info (returned as an array)
+    let desc = breedInfo[0].description; // access first item in array [0] to get description value of object
+    callback(null, desc);
+  });
 };
 
-request(options, function(error, response, body) {
-  if (error) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    return;
-  }
-  let breedInfo = JSON.parse(body); // asign variable to returned body info (returned as an array)
-  console.log(breedInfo[0].description); // access first item in array [0] to get description value of object
-});
+module.exports = { fetchBreedDescription };
